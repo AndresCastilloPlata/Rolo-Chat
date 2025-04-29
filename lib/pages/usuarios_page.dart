2a38e8971110e39ea081bca51d69f8cc623ef4df
AndresCastilloPlata <plataandres26@gmail.com>
2025-04-28 20:23:46 -0500

    se instala dependencia y se implementa el pull to refresh

--- lib/pages/usuarios_page.dart
+++ lib/pages/usuarios_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:rolo_chat/models/usuario.dart';
 
 class UsuariosPage extends StatefulWidget {
@@ -9,6 +10,10 @@ class UsuariosPage extends StatefulWidget {
 }
 
 class _UsuariosPageState extends State<UsuariosPage> {
+  final RefreshController _refreshController = RefreshController(
+    initialRefresh: false,
+  );
+
   final usuarios = [
     Usuario(uid: '1', nombre: 'Andres', email: 'test1@test.com', online: true),
     Usuario(uid: '2', nombre: 'Marina', email: 'test2@test.com', online: false),
@@ -36,29 +41,52 @@ class _UsuariosPageState extends State<UsuariosPage> {
           ),
         ],
       ),
-      body: ListView.separated(
-        physics: const BouncingScrollPhysics(),
-        itemBuilder:
-            (_, index) => ListTile(
-              title: Text(usuarios[index].nombre),
-              leading: CircleAvatar(
-                child: Text(usuarios[index].nombre.substring(0, 2)),
-              ),
-              trailing: Container(
-                width: 10,
-                height: 10,
-                decoration: BoxDecoration(
-                  color:
-                      usuarios[index].online
-                          ? Colors.greenAccent[400]
-                          : Colors.redAccent[700],
-                  borderRadius: BorderRadius.circular(100),
-                ),
-              ),
-            ),
-        separatorBuilder: (_, index) => const Divider(),
-        itemCount: usuarios.length,
+      body: SmartRefresher(
+        controller: _refreshController,
+        enablePullDown: true,
+        onRefresh: _cargarUsuarios,
+        header: WaterDropHeader(
+          waterDropColor: Colors.blue[400]!,
+          complete: Icon(Icons.check, color: Colors.blue[400]!),
+        ),
+        child: _listViewUsuarios(),
       ),
     );
+  }
+
+  ListView _listViewUsuarios() {
+    return ListView.separated(
+      physics: const BouncingScrollPhysics(),
+      itemBuilder: (_, index) => _usuarioListTile(usuarios[index]),
+      separatorBuilder: (_, index) => const Divider(),
+      itemCount: usuarios.length,
+    );
+  }
+
+  ListTile _usuarioListTile(Usuario usuario) {
+    return ListTile(
+      title: Text(usuario.nombre),
+      subtitle: Text(usuario.email),
+      leading: CircleAvatar(
+        backgroundColor: Colors.blue[100],
+        child: Text(usuario.nombre.substring(0, 2)),
+      ),
+      trailing: Container(
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+          color:
+              usuario.online ? Colors.greenAccent[400] : Colors.redAccent[700],
+          borderRadius: BorderRadius.circular(100),
+        ),
+      ),
+    );
+  }
+
+  _cargarUsuarios() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
   }
 }
